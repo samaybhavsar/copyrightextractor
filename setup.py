37cde82ffafede530b13d62faf792726ffad6f5e
Samay Bhavsar <samaybhavsar@gmail.com>
2018-02-13 22:15:12 +0530

    Updated setup.py with version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='copyrightextractor',  # Required
-    version='0.0.1',  # Required
+    version='0.0.2',  # Required
     description='Extract copyright from HTML content',  # Required
     long_description=long_description,  # Optional
     url='https://github.com/samaybhavsar/copyrightextractor',  # Optional
